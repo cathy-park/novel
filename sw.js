5c2c369d3ef667c1cbc76c3f55747dbb742f4d22
apple <apple@0ui-iMac.local>
2026-07-08 14:17:16 +0900

    chore: force service worker update for new features

--- sw.js
+++ sw.js
@@ -37,3 +37,4 @@ self.addEventListener('activate', event => {
 // updated Wed Jul  8 13:55:19 KST 2026
 // updated Wed Jul  8 13:59:12 KST 2026
 // updated Wed Jul  8 14:03:52 KST 2026
+// feature updated Wed Jul  8 14:17:16 KST 2026
